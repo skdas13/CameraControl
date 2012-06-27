ca1f00557b1731a666cb416c2023d1639763872a
skdas13 <skdas13@gmail.com>
2012-06-27 15:58:39 +0100

    initial commit

---
+++ src/com/develogical/camera/Camera.java
@@ -0,0 +1,55 @@
+package com.develogical.camera;
+
+public class Camera implements WriteListener {
+
+    private Sensor sensor;
+    private MemoryCard memoryCard;
+
+    private volatile boolean isPowerOn = false;
+    private volatile boolean isCopying = false;
+    private volatile boolean isPoweringOff = false;
+
+    public Camera(Sensor sensor, MemoryCard memoryCard) {
+        this.sensor = sensor;
+        this.memoryCard = memoryCard;
+    }
+
+    public void pressShutter() {
+        if (isPowerOn()) {
+            isCopying = true; // will be reset to false by memoryCard calling writeComplete()
+            byte[] data = sensor.readData();
+            memoryCard.write(data);
+        }
+    }
+
+    public boolean isPowerOn() {
+        return isPowerOn;
+    }
+
+    public void powerOn() {
+        sensor.powerUp();
+        isPowerOn = true;
+    }
+
+    public void powerOff() {
+        if (!isCopying)
+        {
+            sensor.powerDown();
+            isPowerOn = false;
+        }
+        else
+        {
+            isPoweringOff = true;
+        }
+    }
+
+    @Override
+    public void writeComplete() {
+        isCopying = false;
+
+        if (isPoweringOff) {
+            isPoweringOff = false;
+            powerOff();
+        }
+    }
+}
---
+++ test/com/develogical/camera/TestCamera.java
@@ -0,0 +1,107 @@
+package com.develogical.camera;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(value = JMock.class)
+public class TestCamera {
+
+    private Mockery context = new Mockery();
+
+    private Sensor sensor = context.mock(Sensor.class);
+    private MemoryCard memoryCard = context.mock(MemoryCard.class);
+
+    private Camera camera = new Camera(sensor, memoryCard);
+
+    @Test
+    public void switchingTheCameraOnPowersUpTheSensor() {
+        powerOnCamera();
+    }
+
+    private void powerOnCamera() {
+        context.checking(new Expectations() {{
+            oneOf(sensor).powerUp();
+        }});
+
+        camera.powerOn();
+    }
+
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        powerOffCamera();
+    }
+
+    private void powerOffCamera() {
+        context.checking(new Expectations() {{
+            oneOf(sensor).powerDown();
+        }});
+
+        camera.powerOff();
+    }
+
+    @Test
+    public void pressingTheShutterWhenPowerIsOffDoesNothing() {
+        context.checking(new Expectations() {{
+        }});
+
+        camera.pressShutter();
+    }
+
+    @Test
+    public void pressingTheShutterWhenPowerIsOnCopiesDataFromSensorToMemoryCard() {
+        final byte [] data = new byte[] {};
+
+        powerOnCamera();
+
+        context.checking(new Expectations() {{
+            oneOf(sensor).readData(); will(returnValue(data));
+            oneOf(memoryCard).write(with(same(data)));
+        }});
+
+        camera.pressShutter();
+    }
+
+    @Test
+    public void switchingCameraOffDoesNotPowerDownSensorIfCopyingToMemoryCard() {
+        final byte [] data = new byte[] {};
+
+        powerOnCamera();
+
+        context.checking(new Expectations() {{
+            oneOf(sensor).readData(); will(returnValue(data));
+            oneOf(memoryCard).write(with(same(data)));
+        }});
+
+        camera.pressShutter();
+
+        context.checking(new Expectations() {{
+            never(sensor).powerDown();
+        }});
+
+        camera.powerOff();
+    }
+
+    @Test
+    public void switchingCameraOffPowersDownSensorIfCopyingToMemoryCardCompleted() {
+        final byte [] data = new byte[] {};
+
+        powerOnCamera();
+
+        context.checking(new Expectations() {{
+            oneOf(sensor).readData(); will(returnValue(data));
+            oneOf(memoryCard).write(with(same(data)));
+        }});
+
+        camera.pressShutter();
+        camera.writeComplete();
+
+        context.checking(new Expectations() {{
+            oneOf(sensor).powerDown();
+        }});
+
+        camera.powerOff();
+    }
+}
